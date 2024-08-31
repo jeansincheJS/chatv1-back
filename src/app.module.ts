21afd24715df08acc21ca8bd5e8b1f47ed12f44b
JeanS <jean.sinche@alarcongroup.us>
2024-08-31 01:45:08 -0500

    add mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,19 @@ import { AppService } from './app.service';
 import { WsModule } from './ws/ws.module';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/env.config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
     }),
-    WsModule
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      dbName: process.env.MONGO_DB,
+    }),
+    WsModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
